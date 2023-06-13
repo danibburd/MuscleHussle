7c99137c3d0bfe12266b3ce26a5f375a048c33c0
danielburdick <danielbjarni@gmail.com>
2023-06-13 11:45:13 +0200

    fixed PID

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from read_temp import init_temp_sensor, read_temp
 from pid import PID
 
 #initialize PID
-pid = PID(1.0, 0.1, 0.05, setpoint=17)
+pid = PID(1.0, 1.0, 0.2, setpoint=17)
 pid.tunings = (-1.0, -0.1, 0)
-pid.output_limits = (0, 150)
+pid.output_limits = (0, 10)
 
 #initialize Input/Output devices
 temp_sens = init_temp_sensor()
@@ -59,20 +59,22 @@ def change_voltage(On5):
         cooling_volts.value(0)
 
 def get_change(error):
-    if(error >= 5 ):
+    if(error >= 2 ):
         if(cooling_volts.value() == 0):
             change_voltage(True)
-        error = (error - 5) * 2
+        error = (error - 2) * 42
         freq = error + 250
         if (freq > 666):
             freq = 666
         change_flowrate(freq)
+        if(cooling_volts.value() == 1):
+            change_voltage(False)
         print('freq: ' + str(freq) )
     else:
         if(step_pin2.freq != 0):
-            change_flowrate(10)
-        if(cooling_volts.value() == 1):
-            change_voltage(False)
+            change_flowrate(250)
+        if(cooling_volts.value() == 0):
+            change_voltage(True)
         print('no change')
 
 #OD sensor control
